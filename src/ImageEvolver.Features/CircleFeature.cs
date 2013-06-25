da17e658cdfa5c9984653387c7e72d744577ba05
Øystein Krog <oystein.krog@gmail.com>
2013-06-25 22:29:48 +0200

    Add CircleFeature

---
+++ src/ImageEvolver.Features/CircleFeature.cs
@@ -0,0 +1,53 @@
+#region Copyright
+
+//     ImageEvolver
+//     Copyright (C) 2013-2013 Øystein Krog
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+// 
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System.Collections.Generic;
+using ImageEvolver.Core.Mutation;
+
+namespace ImageEvolver.Features
+{
+    public sealed class CircleFeature : IFeatureWithSubFeatures
+    {
+        public CircleFeature(PointFeature point, ColorFeature color)
+        {
+            Point = point;
+            Color = color;
+        }
+
+        public ColorFeature Color { get; private set; }
+        public PointFeature Point { get; private set; }
+
+        public IFeature Clone()
+        {
+            var clonedPoint = Point.Clone();
+            var clonedColor = Color.Clone();
+            return new CircleFeature((PointFeature) clonedPoint, (ColorFeature) clonedColor);
+        }
+
+        public IEnumerable<IFeature> Features
+        {
+            get
+            {
+                yield return Color;
+                yield return Point;
+            }
+        }
+    }
+}
