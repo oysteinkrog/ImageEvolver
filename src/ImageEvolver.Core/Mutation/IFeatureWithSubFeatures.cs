5af2788d53b636c6c4ea98e0f2be01a06b15171f
Øystein Krog <oystein.krog@gmail.com>
2013-06-25 22:29:11 +0200

    Minor refactoring related to interface hierarchy

--- src/ImageEvolver.Core/Mutation/IFeatureWithSubFeatures.cs
+++ src/ImageEvolver.Core/Mutation/IFeatureWithSubFeatures.cs
@@ -18,12 +18,9 @@
 
 #endregion
 
-using System.Collections.Generic;
-
 namespace ImageEvolver.Core.Mutation
 {
-    public interface IFeatureWithSubFeatures : IFeature
+    public interface IFeatureWithSubFeatures : IFeature, IFeatures
     {
-        IEnumerable<IFeature> Features { get; }
     }
 }
---
+++ src/ImageEvolver.Core/Mutation/IFeatures.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ImageEvolver.Core.Mutation
+{
+    public interface IFeatures
+    {
+        IEnumerable<IFeature> Features { get; }
+    }
+}
