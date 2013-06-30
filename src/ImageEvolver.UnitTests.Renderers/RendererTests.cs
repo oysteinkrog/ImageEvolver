bdaa33f7273946b95c9ee78178481cf344a33b5e
Øystein Krog <oystein.krog@gmail.com>
2013-06-30 19:01:31 +0200

    Improve renderer test, add more polygons to render

--- src/ImageEvolver.UnitTests.Renderers/RendererTests.cs
+++ src/ImageEvolver.UnitTests.Renderers/RendererTests.cs
@@ -68,23 +68,32 @@ namespace ImageEvolver.UnitTests.Renderers
         {
             get
             {
+
+                yield return new PolygonFeature(new List<PointFeature>
+                                                {
+                                                    new PointFeature(100, 20),
+                                                    new PointFeature(250, 20),
+                                                    new PointFeature(180, 200),
+                                                    new PointFeature(80, 200)
+                                                },
+                                                new ColorFeature(0, 0, 255, 128));
+
+                yield return new PolygonFeature(new List<PointFeature>
+                                                {
+                                                    new PointFeature(50, 10),
+                                                    new PointFeature(300, 150),
+                                                    new PointFeature(200, 300),
+                                                    new PointFeature(20, 400)
+                                                },
+                                                new ColorFeature(255, 0, 0, 128));
+
                 yield return new PolygonFeature(new List<PointFeature>
                                                 {
                                                     new PointFeature(150, 50),
                                                     new PointFeature(400, 150),
                                                     new PointFeature(130, 400)
                                                 },
-                                                new ColorFeature(0, 255, 0, 200));
-
-                yield return new PolygonFeature(new List<PointFeature>
-                                                {
-                                                    new PointFeature(50, 50),
-                                                    new PointFeature(300, 150),
-                                                    new PointFeature(200, 300),
-                                                    new PointFeature(20, 400)
-                                                },
-                                                new ColorFeature(255, 0, 0, 200));
-
+                                                new ColorFeature(0, 255, 0, 128));
             }
         }
 
